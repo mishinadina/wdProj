d9da5207888df368fbc58684c32e0f9b0226cb24
Mikhail Cherepanov <x239185@wnco.com>
2018-11-13 20:41:10 -0600

    folder rename

--- src/main/java/com/proj/App.java
+++ src/main/java/com/proj/App.java
@@ -1,4 +1,4 @@
-package com.swacorp;
+package com.proj;
 
 /**
  * Hello world!
--- src/test/java/com/proj/AppTest.java
+++ src/test/java/com/proj/AppTest.java
@@ -1,4 +1,4 @@
-package com.swacorp;
+package com.proj;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
